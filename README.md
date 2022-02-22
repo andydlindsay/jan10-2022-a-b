a7738c2546eeb4c9eba85f581520ee89fb4a3d02
Andy Lindsay <andydlindsay@gmail.com>
2022-02-22 12:12:50 -0800

    initial commit; add w7 directory

---
+++ README.md
@@ -0,0 +1,3 @@
+# Table of Contents
+
+* [W07D03 - Immutable Update Patterns](/w07d03)
---
+++ w07d03/pats-pizza/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Welcome to my site</h2>
+    </div>
+  );
+};
+
+export default App;
